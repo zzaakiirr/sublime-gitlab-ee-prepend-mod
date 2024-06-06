286506afab11352522b98aaf3accef869386aa0f
Zakir Dzhamaliddinov <zakir.dzhamaliddinov@gmail.com>
2024-06-06 12:50:47 +0300

    Refactor code & Fix PEP8 issues

--- EePrependMod.py
+++ EePrependMod.py
@@ -1,75 +1,89 @@
 import os
-
-import sublime
 import sublime_plugin
 
 
 class EePrependModCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.validate_file_extension()
+        if not self.is_valid_file():
+            return
 
         if self.is_prepended_module_file_opened():
             original_filepath = self.view.file_name().replace('ee', '')
             return self.view.window().open_file(original_filepath)
 
-        self.set_prepended_module_path()
-        self.set_klass_name()
+        prepended_module_path = self.build_prepended_module_path()
+        klass_name = self.build_klass_name(prepended_module_path)
 
         if not self.is_prepend_mod_line_exist():
-            self.add_prepend_mod_line(edit)
+            self.add_prepend_mod_line(edit, klass_name)
 
-        if not os.path.exists(self.prepended_module_path):
-            create_file(self.prepended_module_path, self.build_prepended_module_skeleton())
+        if not os.path.exists(prepended_module_path):
+            create_file(
+                prepended_module_path,
+                self.build_prepended_module_skeleton(klass_name)
+            )
 
-        self.view.window().open_file(self.prepended_module_path)
+        self.view.window().open_file(prepended_module_path)
 
-    # Setters
-
-    def set_prepended_module_path(self):
+    def build_prepended_module_path(self):
         original_path = self.view.file_name()
         paths = original_path.split('/')
 
-        try:
-            split_folder_index = paths.index('app')
-            split_folder_name = 'app'
-        except ValueError:
-            split_folder_index = paths.index('lib')
-            split_folder_name = 'lib'
+        if 'app' in paths:
+            main_folder_name = 'app'
+        elif 'lib' in paths:
+            main_folder_name = 'lib'
 
-        root_path = '/'.join(paths[:split_folder_index] + [f'ee/{split_folder_name}/'])
+        main_folder_index = paths.index(main_folder_name)
+        root_path = '/'.join(
+            paths[:main_folder_index] + [f'ee/{main_folder_name}/']
+        )
 
-        if split_folder_name == 'app':
+        if main_folder_name == 'app':
             if 'concerns' in paths:
-                self.prepended_module_path = root_path + '/'.join(paths[split_folder_index + 1:-1] + ['ee', paths[-1]])
+                return root_path + '/'.join(
+                    paths[main_folder_index + 1:-1] + ['ee', paths[-1]]
+                )
             else:
-                self.prepended_module_path = root_path + '/'.join([paths[split_folder_index + 1], 'ee'] + paths[split_folder_index + 2:])
-        else:
-            self.prepended_module_path = root_path + '/'.join(['ee'] + paths[split_folder_index + 1:])
+                return root_path + '/'.join(
+                    [paths[main_folder_index + 1], 'ee'] +
+                    paths[main_folder_index + 2:]
+                )
 
-    def set_klass_name(self):
+        return root_path + '/'.join(
+            ['ee'] + paths[main_folder_index + 1:]
+        )
+
+    def build_klass_name(self, prepended_module_path):
         # Finds <klass_path> start index from string (ee/.../ee/<klass_path>)
-        start_index = self.prepended_module_path.rindex('ee') + 2
+        start_index = prepended_module_path.rindex('ee') + 2
         # Filepath has extension `.rb`
-        end_index = self.prepended_module_path.index('.')
+        end_index = prepended_module_path.index('.')
 
-        klass_path = self.prepended_module_path[start_index:end_index]
+        klass_path = prepended_module_path[start_index:end_index]
         # Camelize & glue together with `::`
-        self.klass_name = klass_path.title().replace('_', '').replace('/', '::')[2:]
+        return klass_path.title().replace('_', '').replace('/', '::')[2:]
 
     # Actions
 
-    def add_prepend_mod_line(self, edit):
-        self.view.insert(edit, self.view.size(), f"\n{self.klass_name}.prepend_mod")
+    def add_prepend_mod_line(self, edit, klass_name):
+        self.view.insert(edit, self.view.size(), f"\n{klass_name}.prepend_mod")
         self.view.run_command('save')
 
     # Helper methods
 
-    def validate_file_extension(self):
-        if not '.rb' in self.view.file_name():
-            raise ValueError('Not ruby file')
+    def is_valid_file(self):
+        file_name = self.view.file_name()
+        if not '.rb' in file_name:
+            return False
 
-        if 'ee' in self.view.file_name() and not self.is_prepended_module_file_opened():
-            raise ValueError('Not prepened module path')
+        if 'ee' in file_name and not self.is_prepended_module_file_opened():
+            return False
+
+        if 'app/' not in file_name and 'lib/' not in file_name:
+            return False
+
+        return True
 
     def is_prepended_module_file_opened(self):
         return self.view.file_name().count('ee') == 2
@@ -83,9 +97,9 @@ class EePrependModCommand(sublime_plugin.TextCommand):
 
         return 'prepend_mod' in prelast_line_content
 
-    def build_prepended_module_skeleton(self):
+    def build_prepended_module_skeleton(self, klass_name):
         skeleton = '# frozen_string_literal: true\n\n'
-        namespaces = ['EE'] + self.klass_name.split('::')
+        namespaces = ['EE'] + klass_name.split('::')
         indent = 0
 
         for namespace in namespaces:
@@ -100,9 +114,9 @@ class EePrependModCommand(sublime_plugin.TextCommand):
 
         return skeleton
 
+
 def create_file(filename, data):
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
     with open(filename, 'w') as file:
         file.write(data)
-
